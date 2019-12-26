acaaa282194eb37c7759c50aacad1bbe2bbdc69e
bstafford2017 <b.stafford2017@gmail.com>
2019-12-26 14:13:57 -0600

    start first list building script

---
+++ api/scripts/building/list.php
@@ -0,0 +1,12 @@
+<?php
+    include_once '../../objects/database.php';
+    include_once '../../objects/building.php';
+
+    $database = new Database();
+    $connection = $database->getConnection();
+
+    $building = new Building();
+    $query = building->getBuildings();
+
+
+?>
