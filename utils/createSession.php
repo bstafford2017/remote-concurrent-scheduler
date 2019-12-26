70399f72a3ab8a9f8422ffe4f8ce231340499546
bstafford2017 <b.stafford2017@gmail.com>
2019-12-26 14:41:28 -0600

    add admin choices to navbar

--- utils/createSession.php
+++ utils/createSession.php
@@ -2,5 +2,6 @@
     start_session();
     $data = json_decode(file_get_contents("php://input"));
     $_SESSION['username'] = $data->username;
+    $_SESSION['admin'] = $data->admin;
     $_SESSION['expire'] = time() * 30 * 60;
 ?>
