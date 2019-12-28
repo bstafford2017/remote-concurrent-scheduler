d4eb50cb9ef980d51ac216b990f49d169bf697c4
bstafford2017 <b.stafford2017@gmail.com>
2019-12-28 16:34:25 -0600

    convert to table (not done)

--- js/modules/selector.js
+++ js/modules/selector.js
@@ -1,9 +1,9 @@
 $('#by-week').click(() => {
-    $('#week-by-week').css("display", "block");
+    $('#week-by-week').css("display", "");
     $('#month-by-month').css("display", "none");
 });
 
 $('#by-month').click(() => {
-    $('#month-by-month').css("display", "block");
+    $('#month-by-month').css("display", "");
     $('#week-by-week').css("display", "none");
 });
