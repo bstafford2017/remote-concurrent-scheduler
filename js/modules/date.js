8c5d0197fbd36af3407d13ae8cde0d2ead21df2d
bstafford2017 <b.stafford2017@gmail.com>
2019-12-29 10:37:09 -0600

    nearly finished!

--- js/modules/date.js
+++ js/modules/date.js
@@ -1,27 +1,65 @@
+// For global variables
 let today = new Date();
+let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
-let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-showCalendar(currentMonth, currentYear);
+onload();
+
+$('#by-week').click(() => {
+    getWeek();
+});
+
+$('#by-month').click(() => {
+    showCalendar(currentMonth, currentYear);
+});
+
+function onload() {
+    if($('input[name=\'selector\']:checked').val() === "week"){
+        getWeek();
+        console.log("test");
+    } else {
+        showCalendar(currentMonth, currentYear);
+    }
+}
+
+function clear(){
+    $('#week-by-week').empty();
+    $('#month-by-month').empty();
+    $('#month').empty();
+}
 
 function next() {
     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
     currentMonth = (currentMonth + 1) % 12;
-    $('#month-by-month').empty();
-    $('#month').empty();
+    clear();
     showCalendar(currentMonth, currentYear);
 }
 
 function previous() {
     currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
     currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    $('#month-by-month').empty();
-    $('#month').empty();
+    clear();
     showCalendar(currentMonth, currentYear);
 }
 
-function showCalendar(month, year) {
+function getWeek(){
+    let curr = new Date();
+    $('#week-by-week').append("<tr>");        
+    for (let i = 1; i <= 7; i++) {
+        let first = curr.getDate() - curr.getDay() + i - 1;
+        let day = new Date(curr.setDate(first));
+        console.log(first + " " + day + " " + curr.getDate());
+        if(new Date().getDate() === day.getDate()){
+            $('#week-by-week').append("<td class=\"active\">" + day.getDate() + "</td>");        
+        } else {
+            $('#week-by-week').append("<td>" + day.getDate() + "</td>");        
+        }
+    }
+    $('#week-by-week').append("</tr>");
+}
 
+function showCalendar(month, year) {
+    clear();
     $('#month').append(months[currentMonth] + "<br/><span class=\"active\" style=\"font-size: 18px\"></span>" + currentYear);
 
     let firstDay = (new Date(year, month)).getDay();
