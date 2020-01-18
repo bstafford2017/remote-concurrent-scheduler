482ad98fac655e19aba2fce1f9ea91c04984f428
bstafford2017 <bstafford2017@gmail.com>
2020-01-18 13:12:50 -0600

    added time scale

--- js/modules/date.js
+++ js/modules/date.js
@@ -5,78 +5,24 @@ let currentMonth = today.getMonth()
 let currentYear = today.getFullYear()
 let currentDate = today.getDate() - 1
 let week = [0, 0, 0, 0, 0, 0, 0]
+$(".scale").hide()
 showMonthCalendar()
-
-/* Handles click event for day */
-/*$('.valid').mouseenter((event) => {
-    $('#' + event.target.id + '>.month-event').hide()
-    $('#' + event.target.id + '>.week-event').hide()
-    $('#' + event.target.id).append("<div class=\"item\">Create Event</div>")
-    $('#' + event.target.id).append("<div class=\"item\">See More</div>")
-})
-
-$('.valid').mouseleave((event) => {
-    $('.item').remove()
-    $('#' + event.target.id + '>.month-event').show()
-    $('#' + event.target.id + '>.week-event').show()
-})*/
-
-// Handles clicked event
-let clicked = false;
-let previousId = -1
-$(document).click((event) => {
-    console.log(event.target.id)
-    // Check if id and class are defined
-    if(event.target.id && $(event.target).attr('class')){
-        // Check if cell is clicked
-        if($(event.target).attr('class').includes("valid")){
-            // If something is not selected (clicked)
-            if(!clicked){
-                $('#' + event.target.id + '>.month-event').hide()
-                $('#' + event.target.id + '>.week-event').hide()
-                $('#' + event.target.id).append("<button class=\'item\'>Create Event</button>")
-                $('#' + event.target.id).append("<button class=\'item\'>See More</button>")
-                clicked = true;
-            } else {
-
-                // If clicked same cell
-                if(event.target.id === previousId){
-                    // now clickable buttons
-                } else {
-                    $('#' + previousId + '>.week-event').show()
-                    $('#' + previousId + '>.month-event').show()
-                    $('.item').remove()
-                    clicked = false
-                }
-            }
-        } else {
-            $('#' + previousId + '>.week-event').show()
-            $('#' + previousId + '>.month-event').show()
-            $('.item').remove()
-            clicked = false
-        }
-    } else {
-        $('#' + previousId + '>.week-event').show()
-        $('#' + previousId + '>.month-event').show()
-        $('.item').remove()
-        clicked = false
-    }
-    previousId = event.target.id
-})
 
 // Handles click event for changing displays
 $('#by-week').click(() => {
     clear()
+    $(".scale").show();
     showWeekCalendar(true)
 })
 
 $('#by-month').click(() => {
     clear()
+    $(".scale").hide()
     showMonthCalendar()
 })
 
 function clear(){
-    $('.week-by-week').empty()
+    $('.row').remove()
     $('.month-by-month').empty()
     $('#month').empty()
 }
@@ -178,6 +124,20 @@ function printWeek(date){
     } else {
         $("#0").append("<div id=\"" + date + "\" class=\"valid\">" + date)        
     }
+
+    // Print time scale
+    /*$("#" + date).append("<div id=\"" + date + "-scale\" class=\"scale\">")        
+    for(let i = 6; i < 20; i++){
+        if(i === 12){
+            $("#" + date + "-scale").append("<div id=\"" + date + "-12p\">12p</div>")
+            continue
+        }
+        if(i > 12){
+            $("#" + date + "-scale").append("<div id=\"" + date + "-" + i % 12 + "p\">" + i % 12 + "p</div>")
+        } else {
+            $("#" + date + "-scale").append("<div id=\"" + date + "-" + i % 12 + "a\">" + i % 12 + "a</div>")
+        }
+    }*/
     /*$.ajax({
         type: "get",
         url: "../../api/scripts/event/list.php",
@@ -192,12 +152,12 @@ function printWeek(date){
         }
     })*/
     $("#0").append("</div>")
-    $("#" + date).append("<div class=\"week-event\">9pm - ACM Meeting</div>")
+    $("#" + date).append("<div class=\"week-event\">3pm - ACM Meeting</div>")
 }
 
 // BUG: clicking back and forth with by week and by month
 function showWeekCalendar(positive){
-    $('.week-by-week').empty()
+    $('.row').remove()
     $('.week-by-week').append("<div id=\"0\" class=\"row\">")
 
     // If on today's week, keep first as this
@@ -262,7 +222,7 @@ function printMonth(active, valid, row, date){
     
             }
         })*/
-        $("#" + date).append("<div class=\"month-event\">9pm - ACM Meeting</div>")
+        $("#" + date).append("<div class=\"month-event\">3pm - ACM Meeting</div>")
     }
 }
 
---
+++ js/modules/selector.js
@@ -0,0 +1,57 @@
+let clicked = false
+let previousId = -1
+
+/* Handles click event for day */
+/*$('.valid').mouseenter((event) => {
+    $('#' + event.target.id + '>.month-event').hide()
+    $('#' + event.target.id + '>.week-event').hide()
+    $('#' + event.target.id).append("<div class=\"item\">Create Event</div>")
+    $('#' + event.target.id).append("<div class=\"item\">See More</div>")
+})
+
+$('.valid').mouseleave((event) => {
+    $('.item').remove()
+    $('#' + event.target.id + '>.month-event').show()
+    $('#' + event.target.id + '>.week-event').show()
+})*/
+
+// Handles clicked event
+$(document).click((event) => {
+    console.log(event.target.id)
+    // Check if id and class are defined
+    if(event.target.id && $(event.target).attr('class')){
+        // Check if cell is clicked
+        if($(event.target).attr('class').includes("valid")){
+            // If something is not selected (clicked)
+            if(!clicked){
+                $('#' + event.target.id + '>.month-event').hide()
+                $('#' + event.target.id + '>.week-event').hide()
+                $('#' + event.target.id).append("<button class=\'item\'>Create Event</button>")
+                $('#' + event.target.id).append("<button class=\'item\'>See More</button>")
+                clicked = true;
+            } else {
+
+                // If clicked same cell
+                if(event.target.id === previousId){
+                    // now clickable buttons
+                } else {
+                    $('#' + previousId + '>.week-event').show()
+                    $('#' + previousId + '>.month-event').show()
+                    $('.item').remove()
+                    clicked = false
+                }
+            }
+        } else {
+            $('#' + previousId + '>.week-event').show()
+            $('#' + previousId + '>.month-event').show()
+            $('.item').remove()
+            clicked = false
+        }
+    } else {
+        $('#' + previousId + '>.week-event').show()
+        $('#' + previousId + '>.month-event').show()
+        $('.item').remove()
+        clicked = false
+    }
+    previousId = event.target.id
+})
