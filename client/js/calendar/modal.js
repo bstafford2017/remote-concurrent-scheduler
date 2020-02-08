001ac2759c045a351118e43934e4f07f1f1897f4
b.stafford2017 <b.stafford2017@gmail.com>
2020-02-08 01:08:02 -0600

    added create section to modal

--- client/js/calendar/modal.js
+++
@@ -1,7 +0,0 @@
-let active = $('nav-tabs > li > active')
-$('.nav-tabs > li > a').click((event) => {
-    console.log(event.target.id)
-    active.removeClass('active')
-    $('#' + event.target.id).addClass('active')
-    $('.modal-body').empty()
-}) 
