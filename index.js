dccafe309109537fecd5ad5fa88a32412ef98b16
bstafford2017 <b.stafford2017@gmail.com>
2020-02-15 14:50:31 -0600

    add local dependenies

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const logger = require('./utils/logger')
+const path = require('path')
 
 const app = express()
 
 // Middleware for logging requests
 app.use(logger)
 
-// Set static directory
+// Set static directories
 app.use('/', express.static(__dirname + '/client'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css/'))
+app.use('/bootstrapjs', express.static(__dirname + '/node_modules/bootstrap/js/dist/'))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
+app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist/'))
 
 // Routes for API
 app.use('/api/event', require('./routes/api/event'))
