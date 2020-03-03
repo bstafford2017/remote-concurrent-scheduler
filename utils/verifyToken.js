79625a48a5f900850065e3c4ff16425408bdaa9a
Ben <ben@Bens-iMac.midcoip.net>
2020-03-03 09:27:36 -0600

    small

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 function verifyToken(req, res, next) {
-
     const token = req.cookies.token
     if(!token){
-        // Verify token with jwt
         jwt.verify(token, 'secret-key', (err, authData) => {
             if(err) {
-                console.log(err) 
+                console.log(err)
                 res.redirect('login.html')
             }
             next()
