631f0f075fb95004055f102c78520efb2ba40afb
bstafford2017 <b.stafford2017@gmail.com>
2020-03-05 10:00:42 -0600

    fixed issue with log file

--- utils/logger.js
+++ utils/logger.js
@@ -4,15 +4,16 @@ const path = require('path')
 
 // Middleware function
 const logger = (req, res, next) => {
-  const content = `[${req.method}] ${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format('MMMM Do YYYY, h:mm:ss a')} : ${req.body}`
-  const targetPath = path.join('./logs', 'log.txt')
-  // Save to log file
-  if(fs.existsSync(targetPath)){
-    fs.appendFile(targetPath, content + '\n', (err) => {if(err) throw err})
-  } else {
-    fs.writeFile(targetPath, content, (err) => {if(err) throw err})
-  }
-  next()
+    const content = `[${req.method}] ${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format('MMMM Do YYYY, h:mm:ss a')} : ${req.body}`
+    const targetPath = path.join('./logs', 'log.txt')
+    // Save to log file
+    if(fs.existsSync(targetPath)){
+        fs.appendFile(targetPath, content + '\n', (err) => {if(err) throw err})
+    } else {
+        fs.mkdirSync('./logs')
+        fs.writeFile(targetPath, content, (err) => {if(err) throw err})
+    }
+    next()
 }
 
 module.exports = logger
