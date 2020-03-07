3c89b0789c093645b3ac5ca7e836ba1023353dc5
bstafford2017 <b.stafford2017@gmail.com>
2020-03-07 10:04:47 -0600

    add days of the week in modal

--- public/js/modal/recur.js
+++ public/js/modal/recur.js
@@ -1,3 +1,22 @@
 $('#recur').change(() => {
-    $('#recur').is(':checked') ? $('#recur-end').removeAttr('disabled') : $('#recur-end').attr('disabled', 'disabled')
+    if($('#recur').is(':checked')){
+        $('#recur-end').removeAttr('disabled')
+        $('#sunday').removeAttr('disabled')
+        $('#monday').removeAttr('disabled')
+        $('#tuesday').removeAttr('disabled')
+        $('#wednesday').removeAttr('disabled')
+        $('#thursday').removeAttr('disabled')
+        $('#friday').removeAttr('disabled')
+        $('#saturday').removeAttr('disabled')
+    } else {
+        $('#recur-end').attr('disabled', 'disabled')
+        $('#sunday').attr('disabled', 'disabled')
+        $('#monday').attr('disabled', 'disabled')
+        $('#tuesday').attr('disabled', 'disabled')
+        $('#wednesday').attr('disabled', 'disabled')
+        $('#thursday').attr('disabled', 'disabled')
+        $('#friday').attr('disabled', 'disabled')
+        $('#saturday').attr('disabled', 'disabled')
+    }
+
 })
