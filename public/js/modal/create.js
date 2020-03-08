fce0c0d319ca13c3736f76c8bd8eed1dd4d9a610
bstafford2017 <b.stafford2017@gmail.com>
2020-03-08 10:26:00 -0500

    display after create!

--- public/js/modal/create.js
+++ public/js/modal/create.js
@@ -18,12 +18,16 @@ $('#create-event').click(event => {
             start,
             end,
             recur
-        }
-        ,success: function(response){
+        },
+        success: function(response){
             $('.view').append(
                 `<div class="" id="${response.results.id}">
                     <input type="text" class="text form-control col-10 d-inline" value="${response.results.title}">
-                </div>`)        },
+                </div>`)
+            let day = parseInt(response.results.date.substring(8,10))
+            $("#" + day).append(`<div data-toggle=\"tooltip\" data-placement=\"top\" title=\"Tooltip on top\" class=\"month-event\">${response.results.title}</div>`)       
+            $('#myModal').modal('hide')
+        },
         error: function(response){
             $('#alert').empty()
             $('#alert').append(response)
