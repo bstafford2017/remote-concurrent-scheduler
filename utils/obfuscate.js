baef14de3df9c3e0090f54bcda59d77d2e721729
bstafford2017 <b.stafford2017@gmail.com>
2020-04-12 21:58:27 -0500

    update obfuscate, not working function

--- utils/obfuscate.js
+++ utils/obfuscate.js
@@ -1,19 +1,40 @@
 const fs = require('fs')
 const obfuscator = require('javascript-obfuscator')
+const { exec } = require('child_process');
 
-function obfuscate(req, res, next) {
-    /*fs.readFile(__dirname + '../obfuscate/' + req.url, 'UTF-8', (err, data) => {
+function shell(cmd) {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (err, stdout, stderr) => {
+            if(err)
+                throw err;
+            resolve(stdout ? stdout : stderr);
+        });
+    })
+}
+
+async function obfuscate(req, res, next) {
+    if(!fs.existsSync(__dirname + '/../obfuscate')) {
+        try {
+            console.log('starting shell cmd')
+            await shell('npm run obfuscate')
+            console.log('ending shell cmd')
+        } catch(err) {
+            throw err
+        }
+    }
+    console.log('reading file')
+    fs.readFile(__dirname + '/../obfuscate' + req.url, 'UTF-8', (err, data) => {
         if(err)
             throw err
         res.send(data)
-    })*/
-    // ONLY FOR DEV!!!
-    fs.readFile(__dirname + '/../public/js' + req.url, 'UTF-8', (err, data) => {
-        if(err)
-            throw err
-        const result = obfuscator.obfuscate(data)
-        res.send(result.getObfuscatedCode())
     })
+    // ONLY FOR DEV!!!
+    // fs.readFile(__dirname + '/../public/js' + req.url, 'UTF-8', (err, data) => {
+    //     if(err)
+    //         throw err
+    //     const result = obfuscator.obfuscate(data)
+    //     res.send(result.getObfuscatedCode())
+    // })
 
 }
 module.exports = obfuscate
