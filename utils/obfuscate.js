c2cd905651d81c43a1a8d18af1a31934ca49dc33
bstafford2017 <b.stafford2017@gmail.com>
2020-04-12 22:26:21 -0500

    working without nodemon

--- utils/obfuscate.js
+++ utils/obfuscate.js
@@ -13,28 +13,26 @@ function shell(cmd) {
 }
 
 async function obfuscate(req, res, next) {
-    if(!fs.existsSync(__dirname + '/../obfuscate')) {
+    // NOTE: This does NOT work with nodemon
+    /*if(!fs.existsSync(__dirname + '/../obfuscate')) {
         try {
-            console.log('starting shell cmd')
-            await shell('npm run obfuscate')
-            console.log('ending shell cmd')
+            console.log(await shell('npm run obfuscate'))
         } catch(err) {
             throw err
         }
     }
-    console.log('reading file')
     fs.readFile(__dirname + '/../obfuscate' + req.url, 'UTF-8', (err, data) => {
         if(err)
             throw err
         res.send(data)
-    })
+    })*/
     // ONLY FOR DEV!!!
-    // fs.readFile(__dirname + '/../public/js' + req.url, 'UTF-8', (err, data) => {
-    //     if(err)
-    //         throw err
-    //     const result = obfuscator.obfuscate(data)
-    //     res.send(result.getObfuscatedCode())
-    // })
+    fs.readFile(__dirname + '/../public/js' + req.url, 'UTF-8', (err, data) => {
+        if(err)
+            throw err
+        const result = obfuscator.obfuscate(data)
+        res.send(result.getObfuscatedCode())
+    })
 
 }
 module.exports = obfuscate
