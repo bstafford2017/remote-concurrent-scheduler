53c83fa1341bbba66efcb6cec44d8cb7b453d53c
b.stafford2017 <b.stafford2017@gmail.com>
2020-04-26 15:52:15 -0500

    allow colons for time in filter function

--- utils/filter.js
+++ utils/filter.js
@@ -1,5 +1,5 @@
 function filter(str){
-    return str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
+    return str.replace(/[`~!@#$%^&*()_|+\-=?;'",.<>\{\}\[\]\\\/]/gi, '')
 }
 
 module.exports = filter
