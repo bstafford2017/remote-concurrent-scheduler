8c6d9732d7901f5153862804bc3e0ecd9d1ded01
b.stafford2017 <b.stafford2017@gmail.com>
2020-05-04 15:30:22 -0500

    remove the _ in the filter for matchWeek

--- utils/filter.js
+++ utils/filter.js
@@ -1,6 +1,6 @@
 function filter(str){
     str = str.toString()
-    return str.replace(/[`~!@#$%^&*()_|+=?;'",.<>\{\}\[\]\\]/gi, '')
+    return str.replace(/[`~!@#$%^&*()|+=?;'",.<>\{\}\[\]\\]/gi, '')
 }
 
 module.exports = filter
