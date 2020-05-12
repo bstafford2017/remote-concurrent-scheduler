605ea114ee79f9893c6a51f2bb9def2be108a9dd
Benjamin Stafford <benjaminstafford@Benjamins-MacBook-Pro.local>
2020-05-12 14:56:09 -0500

    async/await dump

--- utils/dump.js
+++ utils/dump.js
@@ -1,19 +1,21 @@
 const mysqldump = require('mysqldump')
 const log = require('./log')
 
-function dump() {
-    mysqldump({
-        connection: {
-            host     : '127.0.0.1',
-            user     : 'root',
-            password : 'root',
-            database : 'rcs'
-        },
-        dumpToFile: 'media/pi/*/full-database.gz',
-        compressFile: true
-    }).catch(err => {
+async function dump() {
+    try {
+        await mysqldump({
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'root',
+                password : 'root',
+                database : 'rcs'
+            },
+            dumpToFile: 'media/pi/*/full-database.gz',
+            compressFile: true
+        })
+    } catch(err) {
         log('error-log', err.toString())
-    })
+    }
 }
 
 module.exports = dump
