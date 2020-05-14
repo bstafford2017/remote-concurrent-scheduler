3a6fc8580c8e92c6ed93751f9011439a3ebf117a
Benjamin Stafford <benjaminstafford@Benjamins-MacBook-Pro.local>
2020-05-14 12:49:25 -0500

    add check to piPath with dump

--- utils/dump.js
+++ utils/dump.js
@@ -1,8 +1,10 @@
 const mysqldump = require('mysqldump')
+const fs = require('fs')
 const log = require('./log')
 
 async function dump() {
     try {
+        const piPath = 'media/pi/*/full-database.gz'
         await mysqldump({
             connection: {
                 host     : '127.0.0.1',
@@ -10,7 +12,7 @@ async function dump() {
                 password : 'root',
                 database : 'rcs'
             },
-            dumpToFile: 'media/pi/*/full-database.gz',
+            dumpToFile: (fs.existsSync(piPath)) ? piPath : 'logs/full-database.gz',
             compressFile: true
         })
     } catch(err) {
