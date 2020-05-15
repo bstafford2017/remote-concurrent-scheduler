f8342dd5cdaea49597406e1daf0c945d864421ff
Benjamin Stafford <benjaminstafford@Benjamins-MacBook-Pro.local>
2020-05-15 13:51:06 -0500

    update dump

--- utils/dump.js
+++ utils/dump.js
@@ -4,7 +4,6 @@ const log = require('./log')
 
 async function dump() {
     try {
-        const piPath = 'media/pi/*/full-database.gz'
         await mysqldump({
             connection: {
                 host     : '127.0.0.1',
@@ -12,7 +11,7 @@ async function dump() {
                 password : 'root',
                 database : 'rcs'
             },
-            dumpToFile: (fs.existsSync(piPath)) ? piPath : 'logs/full-database.gz',
+            dumpToFile: 'logs/full-database.gz',
             compressFile: true
         })
     } catch(err) {
