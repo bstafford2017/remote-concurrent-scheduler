6e0abe81144cf77534e1031e32fa75c8f5797090
J Schmitz <51449182+machaman@users.noreply.github.com>
2020-06-09 01:30:53 -0500

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 20s
-sudo -u pi chromium-browser --start-fullscreen http://192.168.4.1:5000/live.html&building=Upson%20II&room=125 || echo "Service Failed"
+sudo -u pi chromium-browser --disable-session-crashed-bubble --start-fullscreen http://192.168.4.1:5000/live.html&building=Upson%20II&room=125 || echo "Service Failed"
