5b907c008e737ef90c14043f0e57a8087f614151
bstafford2017 <b.stafford2017@gmail.com>
2021-08-26 21:12:15 -0500

    Remove writing to local logs

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -9,11 +9,7 @@ export default createLogger({
     format.splat(),
     format.json()
   ),
-  defaultMeta: { service: 'rcs-graphql' },
-  transports: [
-    new transports.File({ filename: './logs/error.log', level: 'error' }),
-    new transports.File({ filename: './logs/info.log' })
-  ]
+  defaultMeta: { service: 'rcs-graphql' }
 }).add(
   new transports.Console({
     format: format.combine(format.colorize(), format.simple())
