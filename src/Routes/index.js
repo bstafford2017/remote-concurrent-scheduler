b6b9fb89bfaffee75a47adcfaf1edccbcef90b9d
bstafford2017 <b.stafford2017@gmail.com>
2022-10-16 18:41:34 -0500

    Fix navigation and root pages

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,11 +1,10 @@
-// Non-protected routes
-export const LOGIN_URL = '/'
+export const BASE_URL = '/remote-concurrent-scheduler'
 
+// Non-protected routes
+export const LOGIN_URL = BASE_URL
 // Protected routes
-export const HOME_URL = '/home'
-export const CREATE_MODAL_URL = HOME_URL + '/create'
-export const VIEW_MODAL_URL = HOME_URL + '/view'
-export const SEARCH_URL = '/search'
-export const SETTINGS_URL = '/settings'
-export const ROOMS_URL = '/rooms'
-export const USERS_URL = '/users'
+export const HOME_URL = BASE_URL + '/home'
+export const SEARCH_URL = BASE_URL + '/search'
+export const SETTINGS_URL = BASE_URL + '/settings'
+export const ROOMS_URL = BASE_URL + '/rooms'
+export const USERS_URL = BASE_URL + '/users'
